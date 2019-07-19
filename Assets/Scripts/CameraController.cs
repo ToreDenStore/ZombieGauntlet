2361ac3ddd86b0ba604bc2e0a0e51ffac9a17a5e
ToreDenStore <jonatan.sjo.jerling@gmail.com>
2019-07-19 11:59:16 +0200

    Set camera follow and angle

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+
+    public GameObject player;
+    private Vector3 offset;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        offset = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    //After physics calculation and stuff
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
     }
 }
