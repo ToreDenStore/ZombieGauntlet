deee61cbb81cdf9ac6bbc83bcf695e65ba809fb5
ToreDenStore <jonatan.sjo.jerling@gmail.com>
2019-07-19 15:50:55 +0200

    Add shooting forward

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,19 +7,35 @@ public class PlayerController : MonoBehaviour
     public float Speed;
     private CharacterController _controller;
 
+    //Shots
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+    private float nextFire;
+
     // Start is called before the first frame update
     void Start()
     {
         _controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
+    {
+        if(Input.GetMouseButtonDown(0) && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal * Time.deltaTime * Speed, 0.0f, moveVertical * Time.deltaTime * Speed);
+        Vector3 movement = new Vector3(moveHorizontal * Speed, 0.0f, moveVertical * Speed);
 
         _controller.SimpleMove(movement);
     }
