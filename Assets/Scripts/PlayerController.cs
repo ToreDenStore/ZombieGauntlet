f4d64462bb1da90dd22c28aad727a15c494dad2c
ToreDenStore <jonatan.sjo.jerling@gmail.com>
2019-07-19 11:44:02 +0200

    Add files

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal * Time.deltaTime * Speed, 0.0f, moveVertical * Time.deltaTime * Speed);
+
+        transform.Translate(movement);
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+}
