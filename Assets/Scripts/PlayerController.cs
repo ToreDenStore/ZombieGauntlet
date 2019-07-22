90207354e343985f02225f5ae4b38e2171ede862
ToreDenStore <jonatan.sjo.jerling@gmail.com>
2019-07-22 12:14:07 +0200

    Reordering code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,60 +10,37 @@ public class PlayerController : MonoBehaviour
     Vector3 mousePosition;
 
     //Shots
-    public GameObject shot;
     public GameObject gun;
     public Transform shotSpawn;
-    public float fireRate;
-    private float nextFire;
-    private WaitForSeconds shotDuration = new WaitForSeconds(0.5f);
-    public float weaponRange = 50f;
-    private LineRenderer laserLine;
     public GameObject shotExplosion;
+
+    public float fireRate;
+    public float weaponRange = 50f;
+
+    private float nextFire;
+    private LineRenderer _laserLine;
 
     // Start is called before the first frame update
     void Start()
     {
         _controller = GetComponent<CharacterController>();
         _cameraOffset = Camera.main.transform.position;
-        laserLine = GetComponent<LineRenderer>();
+        _laserLine = GetComponent<LineRenderer>();
     }
 
+    // Update is called once per frame
     void Update()
     {
         mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - _cameraOffset;
 
         FaceMouse();
 
-        if ((Input.GetMouseButtonDown(0) || Input.GetMouseButton(0)) && Time.time > nextFire)
-        {
-            nextFire = Time.time + fireRate;
-            //Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        Move();
 
-            StartCoroutine(ShotEffect());
-
-            RaycastHit hit;
-            laserLine.SetPosition(0, shotSpawn.position);
-            Vector3 rayOrigin = shotSpawn.position;
-            Vector3 gunDirection = new Vector3(
-                mousePosition.x - shotSpawn.position.x,
-                0,
-                mousePosition.z - shotSpawn.position.z
-            );
-            if (Physics.Raycast(rayOrigin, gunDirection, out hit, weaponRange))
-            {
-                laserLine.SetPosition(1, hit.point);
-                Instantiate(shotExplosion, hit.point, transform.rotation);
-            }
-            else
-            {
-                laserLine.SetPosition(1, rayOrigin + (transform.forward * weaponRange));
-            }
-        }
-        
+        ShootGun();
     }
-
-    // Update is called once per frame
-    void FixedUpdate()
+    
+    void Move()
     {
         //Movement
         float moveHorizontal = Input.GetAxis("Horizontal");
@@ -76,17 +53,42 @@ public class PlayerController : MonoBehaviour
     {
         //Facing mouse
         Vector3 direction = new Vector3(
-                mousePosition.x - transform.position.x,
-                0,
-                mousePosition.z - transform.position.z
+            mousePosition.x - transform.position.x,
+            0,
+            mousePosition.z - transform.position.z
         );
         transform.forward = direction;
     }
 
-    private IEnumerator ShotEffect()
+    void ShootGun()
     {
-        laserLine.enabled = true;
-        yield return shotDuration;
-        laserLine.enabled = false;
+        if ((Input.GetMouseButtonDown(0) || Input.GetMouseButton(0)) && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+
+            //StartCoroutine(ShotEffect());
+
+            RaycastHit hit;
+            _laserLine.SetPosition(0, shotSpawn.position);
+            Vector3 rayOrigin = shotSpawn.position;
+            Vector3 gunDirection = new Vector3(
+                mousePosition.x - shotSpawn.position.x,
+                0,
+                mousePosition.z - shotSpawn.position.z
+            );
+            if (Physics.Raycast(rayOrigin, gunDirection, out hit, weaponRange))
+            {
+                _laserLine.SetPosition(1, hit.point);
+                Instantiate(shotExplosion, hit.point, transform.rotation);
+            }
+            else
+            {
+                _laserLine.SetPosition(1, rayOrigin + (transform.forward * weaponRange));
+            }
+        }
     }
+
+    //private IEnumerator ShotEffect()
+    //{
+    //}
 }
