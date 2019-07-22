18392808a6323e37039f9f19e42697cc1c9dad64
ToreDenStore <jonatan.sjo.jerling@gmail.com>
2019-07-22 10:24:04 +0200

    Make shot die at collide or after 3 seconds

---
+++ Assets/Scripts/ShotCollider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotCollider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+}
--- Assets/Scripts/ShotController.cs
+++ Assets/Scripts/ShotController.cs
@@ -2,19 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Mover : MonoBehaviour
+public class ShotController : MonoBehaviour
 {
     public float speed;
+    public float lifeTimeSeconds;
+    private float lifeTimeStart;
 
     // Start is called before the first frame update
     void Start()
     {
         GetComponent<Rigidbody>().velocity = transform.forward * speed;
+        lifeTimeStart = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Time.time - lifeTimeSeconds > lifeTimeStart)
+        {
+            Destroy(gameObject);
+        }
     }
 }
