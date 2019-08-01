2225291dd4ced41e09ea753bd3bf817dda69fdb3
ToreDenStore <jonatan.sjo.jerling@gmail.com>
2019-08-01 20:56:33 +0200

    Moce camera working

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,12 +6,19 @@ public class CameraController : MonoBehaviour
 {
 
     public GameObject player;
+    public float cameraXOffsetMax;
+    public float cameraZOffsetMax;
+    public float cameraMoveSpeedX;
+    public float cameraMoveSpeedZ;
+
     private Vector3 offset;
+    private Vector3 lastPlayerPosition;
 
     // Start is called before the first frame update
     void Start()
     {
         offset = transform.position;
+        lastPlayerPosition = player.transform.position;
     }
 
     // Update is called once per frame
@@ -23,6 +30,79 @@ public class CameraController : MonoBehaviour
     //After physics calculation and stuff
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+        //Base
+        //transform.position = player.transform.position + offset;
+
+        if (player.transform.position != lastPlayerPosition)
+        {
+            print("player is moving");
+
+            bool playerIsMovingRight = (player.transform.position.x > lastPlayerPosition.x);
+            bool playerIsMovingLeft = (player.transform.position.x < lastPlayerPosition.x);
+
+            float playerPositionX = player.transform.position.x;
+            float cameraPositionX = transform.position.x;
+
+            if (playerIsMovingRight && (cameraPositionX - playerPositionX) <= cameraXOffsetMax)
+            {
+                print("move camera to the right");
+                transform.position = new Vector3(
+                    transform.position.x + (player.transform.position.x - lastPlayerPosition.x) * cameraMoveSpeedX,
+                    transform.position.y,
+                    transform.position.z
+                );
+            } else if (playerIsMovingLeft && (cameraPositionX - playerPositionX) >= -cameraXOffsetMax)
+            {
+                print("move camera to the left");
+                transform.position = new Vector3(
+                    transform.position.x + (player.transform.position.x - lastPlayerPosition.x) * cameraMoveSpeedX,
+                    transform.position.y,
+                    transform.position.z
+                );
+            }
+            else
+            {
+                transform.position = new Vector3(
+                    transform.position.x + player.transform.position.x - lastPlayerPosition.x,
+                    transform.position.y,
+                    transform.position.z
+                );
+            }
+
+            bool playerIsMovingUp = (player.transform.position.z > lastPlayerPosition.z);
+            bool playerIsMovingDown = (player.transform.position.z < lastPlayerPosition.z);
+
+            float playerPositionZ = player.transform.position.z;
+            float cameraPositionZ = transform.position.z;
+
+            if (playerIsMovingUp && (cameraPositionZ - playerPositionZ) <= cameraZOffsetMax)
+            {
+                transform.position = new Vector3(
+                    transform.position.x,
+                    transform.position.y,
+                    transform.position.z + (player.transform.position.z - lastPlayerPosition.z) * cameraMoveSpeedZ
+                );
+            } else if (playerIsMovingDown && (cameraPositionZ - playerPositionZ) >= -cameraZOffsetMax)
+            {
+                transform.position = new Vector3(
+                    transform.position.x,
+                    transform.position.y,
+                    transform.position.z + (player.transform.position.z - lastPlayerPosition.z) * cameraMoveSpeedZ
+                );
+            }
+            else
+            {
+                transform.position = new Vector3(
+                    transform.position.x,
+                    transform.position.y,
+                    transform.position.z + player.transform.position.z - lastPlayerPosition.z
+                );
+            }
+
+            lastPlayerPosition = player.transform.position;
+        } else
+        {
+            print("player is not moving");
+        }
     }
 }
