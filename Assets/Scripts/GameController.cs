1047d1b8b6f2825a6941644360d6094c1bf3e234
ToreDenStore <jonatan.sjo.jerling@gmail.com>
2019-08-05 14:59:45 +0200

    Add background music

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private AudioSource backgroundMusic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        backgroundMusic = GetComponent<AudioSource>();
+        //backgroundMusic.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
