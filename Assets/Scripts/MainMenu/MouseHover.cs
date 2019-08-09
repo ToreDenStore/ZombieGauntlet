d5967537047d0d29698d347991ec7023a4877cca
ToreDenStore <jonatan.sjo.jerling@gmail.com>
2019-08-09 13:37:59 +0200

    First try at 3D menu

---
+++ Assets/Scripts/MainMenu/MouseHover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHover : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void OnMouseEnter()
+    {
+        print("mouse enter");
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+
+    void OnMouseDown()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+}
