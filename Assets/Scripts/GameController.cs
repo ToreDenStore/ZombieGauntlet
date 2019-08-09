3fe8fc2b391d67e904e3412b944522895bac20ee
ToreDenStore <jonatan.sjo.jerling@gmail.com>
2019-08-09 10:19:49 +0200

    Move timer code to its own script and call lose when out of time

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -9,39 +9,21 @@ public class GameController : MonoBehaviour
     //Music
     private AudioSource backgroundMusic;
 
-    //Time
-    private string currentTimeDisplayed = "<time>";
-    private Text timerText;
-
-    public GameObject timerTextObject;
-    public int trainLeavesInMinutes;
-
     // Start is called before the first frame update
     void Start()
     {
-        timerText = timerTextObject.GetComponent<Text>();
         backgroundMusic = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        DateTime nowTime = new DateTime().AddMinutes(trainLeavesInMinutes).AddSeconds(Mathf.RoundToInt(Time.time) * -1);
-        UpdateScreenTextWithNewGameTime(nowTime);
+
     }
 
-    void UpdateScreenTextWithNewGameTime(DateTime gameTime)
+    public void loseGame()
     {
-        string displayTime = getDisplayTime(gameTime);
-        if (!displayTime.Equals(currentTimeDisplayed))
-        {
-            timerText.text = timerText.text.Replace(currentTimeDisplayed, displayTime);
-            currentTimeDisplayed = displayTime;
-        }
+        print("You lost!");
     }
 
-    string getDisplayTime(DateTime time)
-    {
-        return time.Minute.ToString() + ":" + time.Second.ToString().PadLeft(2, '0');
-    }
 }
---
+++ Assets/Scripts/TimerController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour
+{
+    //Time
+    private string currentTimeDisplayed = "<time>";
+    private Text timerText;
+    
+    public int trainLeavesInMinutes;
+    public GameController gameController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= trainLeavesInMinutes * 60)
+        {
+            print("You missed the train!");
+            gameController.loseGame();
+        }
+        DateTime nowTime = new DateTime().AddMinutes(trainLeavesInMinutes).AddSeconds(Mathf.RoundToInt(Time.time) * -1);
+        //nowTime doesn't go down below 1 second, could be done better
+        UpdateScreenTextWithNewGameTime(nowTime);
+    }
+
+    void UpdateScreenTextWithNewGameTime(DateTime gameTime)
+    {
+        string displayTime = getDisplayTime(gameTime);
+        if (!displayTime.Equals(currentTimeDisplayed))
+        {
+            timerText.text = timerText.text.Replace(currentTimeDisplayed, displayTime);
+            currentTimeDisplayed = displayTime;
+        }
+    }
+
+    string getDisplayTime(DateTime time)
+    {
+        return time.Minute.ToString() + ":" + time.Second.ToString().PadLeft(2, '0');
+    }
+}
