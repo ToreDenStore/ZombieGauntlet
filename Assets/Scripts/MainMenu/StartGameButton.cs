123185d230d5c2b5afba7e87cc2e99c503b57f32
ToreDenStore <jonatan.sjo.jerling@gmail.com>
2019-08-09 14:01:04 +0200

    Add start game button

---
+++ Assets/Scripts/MainMenu/StartGameButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGameButton : MonoBehaviour
+{
+    void StartGame()
+    {
+        print("clicked!");
+    }
+}
