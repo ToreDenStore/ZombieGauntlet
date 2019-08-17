9461e67a7063669bd7630c44323a95a30f670806
ToreDenStore <jonatan.sjo.jerling@gmail.com>
2019-08-17 13:51:21 +0200

    Remove print statements

--- Assets/Scripts/FootstepSoundScript.cs
+++ Assets/Scripts/FootstepSoundScript.cs
@@ -40,8 +40,8 @@ public class FootstepSoundScript : MonoBehaviour
                     Vector3.Distance(transform.position, lastPosition) > 0.2
                     )
                 {
-                    print(transform.position);
-                    print(lastPosition);
+                    //print(transform.position);
+                    //print(lastPosition);
                     isMoving = true;
                     lastPosition = transform.position;
                 }
@@ -54,12 +54,12 @@ public class FootstepSoundScript : MonoBehaviour
                 if (isMoving && !footstepAudioSource.isPlaying && Time.timeSinceLevelLoad >= 1)
                 {
                     footstepAudioSource.Play();
-                    print("start play footsteps");
+                    //print("start play footsteps");
                 }
                 if (!isMoving && footstepAudioSource.isPlaying)
                 {
                     footstepAudioSource.Stop();
-                    print("stop play footsteps");
+                    //print("stop play footsteps");
                 }
             }
             else
