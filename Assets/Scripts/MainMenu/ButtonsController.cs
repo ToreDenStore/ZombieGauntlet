93e00d26dc89119b7392ed1e065f5ebc19639b41
ToreDenStore <jonatan.sjo.jerling@gmail.com>
2019-08-17 14:31:37 +0200

    Intro text and game info

--- Assets/Scripts/MainMenu/ButtonsController.cs
+++ Assets/Scripts/MainMenu/ButtonsController.cs
@@ -5,7 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class ButtonsController : MonoBehaviour
 {
+    public GameObject startScreen;
+    public GameObject gameInfoScreen;
+    public GameObject introScreen;
+
     public void StartGame()
+    {
+        print("Go to info screen");
+        startScreen.SetActive(false);
+        gameInfoScreen.SetActive(true);
+    }
+
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && gameInfoScreen.activeSelf == true)
+        {
+            print("Go to intro screen");
+            GoToIntroScreen();
+        } else if (Input.GetMouseButtonDown(0) && introScreen.activeSelf == true)
+        {
+            print("Start game");
+            StartLevel();
+        }
+    }
+
+    private void GoToIntroScreen()
+    {
+        gameInfoScreen.SetActive(false);
+        introScreen.SetActive(true);
+    }
+
+    public void StartLevel()
     {
         print("Loading level 1...");
         SceneManager.LoadScene("Level1");
